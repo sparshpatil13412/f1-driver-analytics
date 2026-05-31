7f49d6e1695aad0237ac063fd54d43683af6663a
sparshpatil13412 <dcpsparshpatil@gmail.com>
2026-05-31 22:55:12 +0530

    UI Improvement

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
